69ee4902b06ba8acdf95563de6a50de18ff35b87
Gavishiddappa <gmgmadagi@gmail.com>
2017-12-07 15:58:29 +0530

    spring boot application initial setup

---
+++ src/main/java/com/gavi/easynotes/EasyNotesApplication.java
@@ -0,0 +1,12 @@
+package com.gavi.easynotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyNotesApplication.class, args);
+	}
+}
